ebda7837648ded0541cc92c337bdcdb64d6682f8
juli4ka1998 <juli4ka1998@gmail.com>
2020-02-05 17:25:29 +0200

    Create toolbar and side drawer

---
+++ src/components/Navigation/NavigationItems/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from '../../../Logo/Logo';
+import NavigationItems from '../../NavigationItems/NavigationItems';
+import BackDrop from '../../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../../hoc/Auxiliary/Auxiliary';
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Auxiliary>
+            <BackDrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Auxiliary>
+    )
+};
+export default sideDrawer;
