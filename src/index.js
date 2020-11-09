efbae987467c99e1b848ac534708ffdeec5c6c3e
juli4ka1998 <juli4ka1998@gmail.com>
2020-11-09 13:52:42 +0200

    Add saga

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import {watchAuth} from "./store/sagas";
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
@@ -18,10 +20,14 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const composeEnhancers = process.env.NODE_ENV === 'development'  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
